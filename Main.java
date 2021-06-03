dd9a8d848ec49b0a29ddbebe2222d19abb709954
Ryu <80870887+SeungwonRyu@users.noreply.github.com>
2021-06-03 16:02:20 +0900

    프로그램 종료 추가, 오류 수정
    forth edit

--- Main.java
+++ Main.java
@@ -12,32 +12,38 @@ import java.util.Iterator;
 import java.util.Scanner;
 
 public class Main {
-	Scanner scan = new Scanner(System.in);
+	static Scanner scan = new Scanner(System.in);
 
-	ShoesData_Casual sd_c = new ShoesData_Casual();
-	ShoesData_Sneakers sd_sn = new ShoesData_Sneakers();
-	ShoesData_Sports sd_sp = new ShoesData_Sports();
+	static ShoesData_Casual sd_c = new ShoesData_Casual();
+	static ShoesData_Sneakers sd_sn = new ShoesData_Sneakers();
+	static ShoesData_Sports sd_sp = new ShoesData_Sports();
 	
-	public void main(String[] args) {
-		System.out.println("-------------------------");
-		System.out.println("     1. 상품 정보 추가");
-		System.out.println("     2. 상품 정보 조회");
-		System.out.println("     3. 상품 정보 수정");
-		System.out.println("     Q. 시스템 종료");
-		System.out.println("-------------------------");
+	public static void main(String[] args) {
+		boolean onoff = true;
 		
-		int select_m = scan.nextInt();
+		while(onoff) {
+			System.out.println("-------------------------");
+			System.out.println("     1. 상품 정보 추가");
+			System.out.println("     2. 상품 정보 조회");
+			System.out.println("     3. 상품 정보 수정");
+			System.out.println("     Q. 시스템 종료");
+			System.out.println("-------------------------");
+			
+			String select_m = scan.next();
 		
-		while(true) {
 			switch(select_m) {
-				case 1: //상품 정보 추가
+				case "1": //상품 정보 추가
 					shoesInput();
 					break;	
-				case 2: //상품 정보 조회
+				case "2": //상품 정보 조회
 					shoesLookup();
 					break;
-				case 3: //상품 정보 수정
+				case "3": //상품 정보 수정
 					shoesModordel();
+					break;
+				case "q", "Q":
+					onoff = false;
+					break;
 				default:
 					System.out.println("번호를 알맞게 입력해주세요.");
 			}
@@ -48,7 +54,7 @@ public class Main {
 	 * author : 유승원
 	 * how : 상품 정보 추가 메소드
 	 */
-	public void shoesInput() {
+	public static void shoesInput() {
 		System.out.println("상품 정보를 추가합니다.");
 		System.out.println("1. 캐주얼화");
 		System.out.println("2. 스니커즈");
@@ -69,7 +75,7 @@ public class Main {
 	 * author : 유승원
 	 * how : 상품 정보 조회 메소드
 	 */
-	public void shoesLookup() {
+	public static void shoesLookup() {
 		System.out.println("상품 정보를 조회합니다.");
 		System.out.println("1. 캐주얼화");
 		System.out.println("2. 스니커즈");
@@ -90,7 +96,7 @@ public class Main {
 	 * author : 유승원
 	 * how : 상품 정보 수정 및 삭제 메소드
 	 */
-	public void shoesModordel() {
+	public static void shoesModordel() {
 		System.out.println("상품 정보를 조회합니다.");
 		System.out.println("1. 캐주얼화");
 		System.out.println("2. 스니커즈");
