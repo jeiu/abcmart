77021603ffe223025353fa77be5412719f9252a5
Ryu <80870887+SeungwonRyu@users.noreply.github.com>
2021-06-03 15:52:04 +0900

    메소드 추가로 메인 클래스 모듈화
    third edit

--- Main.java
+++ Main.java
@@ -1,191 +1,146 @@
-package abcmart;
+/* time : 12:20
+ * author : 유승원
+ * how : 전체적인 동작을 나타내는 가시적 메인 클래스
+ */
+
+package shoesMall;
 
 import java.io.FileWriter;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Iterator;
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
 import java.util.Scanner;
-/*
- Date		'21.06.03 03:43
- 
- How		박준영	로컬 json파일 불러온 후 상품 추가 기능 추가.
- 					로컬 json파일 불러온 후 상품 조회 기능 추가.
- 					로컬 json파일 불러온 후 상품 수정 기능 추가.
- 			유승원	전체적인 틀 구성.
- 			
- Author		박준영,  유승원
- 
- Version	1.0.0
- */
+
 public class Main {
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		ShoesData sd = new ShoesData();
-		JSONParser parser = new JSONParser();
-		JSONObject jsonObject = new JSONObject();
-		
+	Scanner scan = new Scanner(System.in);
+
+	ShoesData_Casual sd_c = new ShoesData_Casual();
+	ShoesData_Sneakers sd_sn = new ShoesData_Sneakers();
+	ShoesData_Sports sd_sp = new ShoesData_Sports();
+	
+	public void main(String[] args) {
 		System.out.println("-------------------------");
 		System.out.println("     1. 상품 정보 추가");
 		System.out.println("     2. 상품 정보 조회");
 		System.out.println("     3. 상품 정보 수정");
-		System.out.println("     4. 매출 정보 조회");
+		System.out.println("     Q. 시스템 종료");
 		System.out.println("-------------------------");
+		
+		int select_m = scan.nextInt();
+		
+		while(true) {
+			switch(select_m) {
+				case 1: //상품 정보 추가
+					shoesInput();
+					break;	
+				case 2: //상품 정보 조회
+					shoesLookup();
+					break;
+				case 3: //상품 정보 수정
+					shoesModordel();
+				default:
+					System.out.println("번호를 알맞게 입력해주세요.");
+			}
+		}
+	}
+	
+	/* time : 15:30
+	 * author : 유승원
+	 * how : 상품 정보 추가 메소드
+	 */
+	public void shoesInput() {
+		System.out.println("상품 정보를 추가합니다.");
+		System.out.println("1. 캐주얼화");
+		System.out.println("2. 스니커즈");
+		System.out.println("3. 운동화");
 		
 		int select = scan.nextInt();
 		
-		switch(select) {
-			case 1: // Add info
-				try {
-					Object savedJson = parser.parse(new FileReader("d:\\test.json"));
-					jsonObject = (JSONObject)savedJson;
-				} catch (FileNotFoundException e) {
-					e.printStackTrace();
-					try {
-						// If file not founded save empty file
-						FileWriter file = new FileWriter("d:\\test.json");
-						file.write(jsonObject.toJSONString());
-						file.flush();
-						file.close();
-					} catch (IOException f) {
-						f.printStackTrace();
-					}
-				} catch (IOException e) {
-					e.printStackTrace();
-				} catch (ParseException e) {
-					e.printStackTrace();
-				}
-		        
-		        /* Print Current Info
-		        System.out.print(jsonObject);
-		        */
-		        
-		        // Add info
-		        list = new JSONArray();
-		        System.out.println("상품명을 입력해 주세요.");
-		        sName = scan.next();
-		        list.add("Name : "+sName);
-		        
-		        System.out.println("상품가격을 입력해 주세요.");
-		        sPrice = scan.next();
-		        list.add("Price : "+sPrice);
-		        
-		        System.out.println("상품분류를 입력해 주세요.");
-		        sClassification = scan.next();
-		        list.add("Classification : "+sClassification);
-		        
-		    	jsonObject.put("Info" + (jsonObject.size() + 1), list);
-		    	
-		    	// Save current info file
-				try {
-					FileWriter file = new FileWriter("d:\\test.json");
-					file.write(jsonObject.toJSONString());
-					file.flush();
-					file.close();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-				System.out.println((jsonObject.size()) + "번째 상품이 추가되었습니다.");
-				break;
-				
-			case 2: // Lookup info
-				try {
-					Object savedJson = parser.parse(new FileReader("d:\\test.json"));
-					jsonObject = (JSONObject)savedJson;
-				} catch (FileNotFoundException e) {
-					e.printStackTrace();
-					try {
-						// If file not founded save empty file
-						FileWriter file = new FileWriter("d:\\test.json");
-						file.write(jsonObject.toJSONString());
-						file.flush();
-						file.close();
-					} catch (IOException f) {
-						f.printStackTrace();
-					}
-				} catch (IOException e) {
-					e.printStackTrace();
-				} catch (ParseException e) {
-					e.printStackTrace();
-				}
-				
-				// Print Current Info
-				System.out.println("조회하실 상품의 번호를 입력해 주세요.");
-				int lookupNum = scan.nextInt();
-				if (lookupNum < 1 || lookupNum - 1 > (int)jsonObject.size()) {
-					System.out.println("올바르지 않은 상품 정보입니다.");
-					break;
-				}
-				
-				System.out.println(lookupNum + "번째 상품 정보입니다.");
-				JSONArray loadList = (JSONArray)jsonObject.get("Info" + lookupNum);
-                Iterator<String>iterator = loadList.iterator();
-                while(iterator.hasNext()) {
-                       System.out.println(iterator.next());
-                }
-				break;
-				
-			case 3: // Modify info
-				try {
-					Object savedJson = parser.parse(new FileReader("d:\\test.json"));
-					jsonObject = (JSONObject)savedJson;
-				} catch (FileNotFoundException e) {
-					e.printStackTrace();
-					try {
-						// If file not founded save empty file
-						FileWriter file = new FileWriter("d:\\test.json");
-						file.write(jsonObject.toJSONString());
-						file.flush();
-						file.close();
-					} catch (IOException f) {
-						f.printStackTrace();
-					}
-				} catch (IOException e) {
-					e.printStackTrace();
-				} catch (ParseException e) {
-					e.printStackTrace();
-				}
-				
-				System.out.println("수정하실 상품의 번호를 입력해 주세요.");
-				int modifyNum = scan.nextInt();
-				if (modifyNum < 1 || modifyNum - 1 > (int)jsonObject.size()) {
-					System.out.println("올바르지 않은 상품 정보입니다.");
-					break;
-				}
-				
-				// Modify info
-		        list = new JSONArray();
-		        System.out.println("상품명을 입력해 주세요.");
-		        sName = scan.next();
-		        list.add("Name : "+sName);
-		        
-		        System.out.println("상품가격을 입력해 주세요.");
-		        sPrice = scan.next();
-		        list.add("Price : "+sPrice);
-		        
-		        System.out.println("상품분류를 입력해 주세요.");
-		        sClassification = scan.next();
-		        list.add("Classification : "+sClassification);
-		        
-		    	jsonObject.put("Info" + modifyNum, list);
-		    	
-		    	// Save current info file
-				try {
-					FileWriter file = new FileWriter("d:\\test.json");
-					file.write(jsonObject.toJSONString());
-					file.flush();
-					file.close();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-				System.out.println(modifyNum + "번째 상품이 수정되었습니다.");
-				break;
-				
-			default: 			
+		if(select == 1) {
+			sd_c.input();
+		} else if(select == 2) {
+			sd_sn.input();
+		} else if(select == 3) {
+			sd_sp.input();
+		}
+	}
+	
+	/* time : 15:40
+	 * author : 유승원
+	 * how : 상품 정보 조회 메소드
+	 */
+	public void shoesLookup() {
+		System.out.println("상품 정보를 조회합니다.");
+		System.out.println("1. 캐주얼화");
+		System.out.println("2. 스니커즈");
+		System.out.println("3. 운동화");
+		
+		int select = scan.nextInt();
+		
+		if(select == 1) {
+			sd_c.lookup();
+		} else if(select == 2) {
+			sd_sn.lookup();
+		} else if(select == 3) {
+			sd_sp.lookup();
+		}
+	}
+	
+	/* time : 15:45
+	 * author : 유승원
+	 * how : 상품 정보 수정 및 삭제 메소드
+	 */
+	public void shoesModordel() {
+		System.out.println("상품 정보를 조회합니다.");
+		System.out.println("1. 캐주얼화");
+		System.out.println("2. 스니커즈");
+		System.out.println("3. 운동화");
+		
+		int select = scan.nextInt();
+		
+		if(select == 1) {
+			int select2 = scan.nextInt();
+			
+			System.out.println("1. 상품 수정");
+			System.out.println("2. 상품 삭제");
+			
+			if(select2 == 1) {
+				sd_c.modify();
+			} else if(select2 == 2) {
+				sd_c.delete();
+			} else {
+				System.out.println("번호를 알맞게 입력해주세요.");
+			}
+			
+		} else if(select == 2) {
+			int select2 = scan.nextInt();
+			
+			System.out.println("1. 상품 수정");
+			System.out.println("2. 상품 삭제");
+			
+			if(select2 == 1) {
+				sd_sn.modify();
+			} else if(select2 == 2) {
+				sd_sn.delete();
+			} else {
+				System.out.println("번호를 알맞게 입력해주세요.");
+			}
+			
+		} else if(select == 3) {
+			int select2 = scan.nextInt();
+			
+			System.out.println("1. 상품 수정");
+			System.out.println("2. 상품 삭제");
+			
+			if(select2 == 1) {
+				sd_sp.modify();
+			} else if(select2 == 2) {
+				sd_sp.delete();
+			} else {
+				System.out.println("번호를 알맞게 입력해주세요.");
+			}
+		} else {
+			System.out.println("번호를 알맞게 입력해주세요.");
 		}
 	}
 }
