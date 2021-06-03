38258d8151f308586f6e8797a3f54ab43c4610a7
pdthswown <80870908+pdthswown@users.noreply.github.com>
2021-06-03 11:45:47 +0900

    ShoesData.class와 ShoesObject.class 생성
    ShoesObject.class
    변수 sName과 sPrice 선언
    
    ShoesData.class
    Scanner 클래스 선언
    ShoesObject 구조체 선언

---
+++ ShoesData.java
@@ -0,0 +1,21 @@
+/* time : 11:39
+ * author : 김태성
+ * how : ShoesObject타입을 선언하여 HashMap 에 저장하는 Class
+ */
+
+package shoesMall;
+
+//스캐너
+import java.util.Scanner;
+
+public class ShoesData {
+	
+	Scanner scan = new Scanner(System.in);
+
+	ShoesObject shoesObject = new ShoesObject();
+	
+	public void input() {
+		
+	}
+	
+}
---
+++ ShoesObject.java
@@ -0,0 +1,11 @@
+/* time : 11:35
+ * author : 김태성
+ * how : ShoesObject 구조체에 상품이름과 상품가격 정보 등록하기 위해서 만든 Class
+ */
+package shoesMall;
+
+public class ShoesObject {
+	String sName;
+	int sPrice;
+
+}
